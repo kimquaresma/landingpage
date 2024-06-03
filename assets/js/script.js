26b4cf5d2256c1f9677ac3c4c533779728af1198
kimquaresma <tkdeo9090@gmail.com>
2024-06-03 15:09:37 +0900

    landing

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,30 @@ $(function(){  //로드후 실행되는 스크립트구현시 사용
     gsap.ticker.lagSmoothing(0)
 
 
+    $(document).mousemove(function(e){
+      x=e.clientX;
+      y=e.clientY;
+    
+      gsap.to('.cursor',{
+        left:x,
+        top:y
+      })
+    })
+
+    $('.introlink').click(function(){
+      lenis.scrollTo('#intro')
+    })
+    $('.aboutlink').click(function(){
+      lenis.scrollTo('#about')
+    })
+    $('.worklink').click(function(){
+      lenis.scrollTo('#work')
+    })
+    $('.contactlink').click(function(){
+      lenis.scrollTo('#contact')
+    })
+    
+
     introSlide = new Swiper('.sc-intro .swiper',{
       loop:true,
       speed:3000,
@@ -31,55 +55,14 @@ $(function(){  //로드후 실행되는 스크립트구현시 사용
       }
     })
   
-    // introTextArr = [
-    //   'Adaptable',
-    //   'excelling',
-    //   'challenges',
-    //   'communication'
-    // ]
 
-    // setInterval(() => {
-    //   textRoll();
-    // }, introTextArr.length*300);
-
-    // textRoll();
-    // function textRoll(){
-    //   for (let index = 0; index < introTextArr.length; index++) {
-    //     setTimeout(() => {
-    //       $('.sc-intro .text-area h2').html(introTextArr[index])
-    //     }, 300*index);
-    //   }
-    // }
-
-
-
-
-    // $(window).on('scroll mousemove', function(e){
-    //   $('.cursor').css('left', e.pageX + 'px');
-    //   $('.cursor').css('top', e.pageY + 'px');
-    //   });
-
-
-
-    // let lastScroll = 0;
-    // $(window).scroll(function(){
-    //   current = $(this).scrollTop();
-      
-    //   if(current > lastScroll){
-    //     $('.header').addClass('hide')
-    //   } else {
-    //     $('.header').removeClass('hide')
-    //   }
-    //   lastScroll = current;
-    // })
-
+    
 
     sloganTl = gsap.timeline({
       scrollTrigger:{
             trigger:".slogan",
             start:"0% 100%",
             end:"100% 0%",
-            markers: true,
             scrub: 0,
           },
     })
@@ -94,7 +77,6 @@ $(function(){  //로드후 실행되는 스크립트구현시 사용
           trigger:$(this),
           start:"0% 100%",
           end:"100% 0%",
-          markers: true,
           scrub: 0,
         },
         width:'90%'
@@ -103,36 +85,13 @@ $(function(){  //로드후 실행되는 스크립트구현시 사용
 
     })
 
-
-
-    // gsap.to('.slogan .top',{
-    //   scrollTrigger:{
-    //     trigger:".video2 .inner .text",
-    //     start:"10% 5%",
-    //     end:"100% 5%",
-    //     markers: true,
-    //     scrub: 10,
-    //   },
-    //   xPercent: 30,
-    // })
-    // gsap.to('.slogan .bot',{
-    //   scrollTrigger:{
-    //     trigger:".video2 .inner .text",
-    //     start:"10% 5%",
-    //     end:"100% 5%",
-    //     markers: true,
-    //     scrub: 10,
-    //   },
-    //   xPercent: -50,
-    // })
-
+    
 
     gsap.to('.sc-projectimage .top',{
       scrollTrigger:{
         trigger:".work .fourth",
         start:"10% 5%",
         end:"100% 5%",
-        markers: true,
         scrub: 10,
       },
       xPercent: 10,
@@ -143,12 +102,12 @@ $(function(){  //로드후 실행되는 스크립트구현시 사용
         trigger:".work .fourth",
         start:"50% 5%",
         end:"100% 5%",
-        markers: true,
         scrub: 10,
       },
       xPercent: -10,
     })
-
+    
+    
 
 
 });
